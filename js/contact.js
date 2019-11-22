79fc4cf59b2a249bc44972b2ade051a6ae63d45a
Shomiks <49363384+Shomiks@users.noreply.github.com>
2019-11-22 13:32:29 +0000

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+function submitForm() {
+    if($( "input[name='firstname']").val() == '' || $("input[name='lastname']").val() == ''){
+        alert('Please enter your first and last name');
+
+        if($( "input[name='firstname']").val() == '') $( "input[name='firstname']")[0].style.backgroundColor = '#FF6C64';
+        if($( "input[name='lastname']").val() == '') $( "input[name='lastname']")[0].style.backgroundColor = '#FF6C64';
+    }
+    else if ($("select[name='select']").val() == null) {
+        alert('Please select one option for type');
+        $( "select[name='select']")[0].style.backgroundColor = '#FF6C64';
+    }
+    else if($("input[name='time']:checked", '#form1').val() == null) {
+        alert("Please choose one option for time");
+    }
+    else {
+        alert("Thanks for submiting!");
+        window.location.reload();
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,37 @@
+function toggleMenu() {
+    var menu = $("#menu")[0];
+    if (menu.style.display === "none" || menu.style.display == '') {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
+function changeLocation(location){
+        window.location.pathname = "EPOS/" + location;
+}
+
+function scrollToTop(){
+        $("html, body").animate({scrollTop: 0}, 1400);
+}
+
+$(document).ready(function () {
+    var menu = $("#menu")[0];
+    if(window.location.pathname != "/EPOS/index.html"){
+        menu.style.display = "block";
+
+        if(window.location.pathname == "/EPOS/about-us.html"){
+            $("#about-us")[0].firstElementChild.style.color = "hotpink"
+        }
+        else if(window.location.pathname == "/EPOS/contact.html"){
+            $("#contact")[0].firstElementChild.style.color = "hotpink"
+        }
+        else {
+            $("#gallery")[0].firstElementChild.style.color = "hotpink"
+        }
+    }
+
+    else {
+        $("#home")[0].firstElementChild.style.color = "hotpink"
+    }
+});
